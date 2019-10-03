cac1203cecebfb554351ae9647f9c605c7a05f05
dmoa <St4nislav003@gmail.com>
2019-10-03 13:54:06 +0100

    diff icon

--- conf.lua
+++ conf.lua
@@ -2,5 +2,5 @@ function love.conf(t)
     t.window.title = "Ice Drop"
     t.window.width = 512
     t.window.height = 512
-    t.window.icon = "assets/imgs/icon.png" 
+    t.window.icon = "assets/imgs/ice.png" 
 end
