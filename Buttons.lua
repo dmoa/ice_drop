789f1c3c11652044ce4b1ecf7ed974e1c9f22446
dmoa <spammyboy@protonmail.com>
2020-02-29 23:31:35 +0000

    better code, that's it

--- Buttons.lua
+++ Buttons.lua
@@ -1,29 +1,37 @@
-local function isClicking(x, y, width, height)
-    local mx, my = lmouse.getPosition()
-    return mx > x and mx < x + width and my > y and my < y + height
+local function mouseOver(x, y, width, height)
+    local mx, my = push:toGame(lmouse.getPosition())
+    return mx and my and mx > x and mx < x + width and my > y and my < y + height
 end
 
 local buttons = {}
 
 
 local htpImg = lg.newImage("assets/imgs/howtoplay.png")
+local startImg = lg.newImage("assets/imgs/start.png")
+
 buttons.buttons = {
     {name = "how to play", image = htpImg,
-     x = gameWidth / 2 - htpImg:getWidth() / 2, y = 0,
+     x = gameWidth / 2 - htpImg:getWidth() / 2, y = 50,
      activate = function()  end},
-    {name = "start", image = lg.newImage("assets/imgs/start.png"), x = 60, y = 100,
+    {name = "start", image = startImg,
+     x = gameWidth / 2 - startImg:getWidth() / 2, y = 100,
      activate = function() end}
 }
 
 function buttons:draw()
     for k, button in ipairs(self.buttons) do
         lg.draw(button.image, button.x, button.y)
+        if mouseOver(button.x, button.y, button.image:getDimensions()) then
+            lg.setColor(0, 0, 0)
+            lg.rectangle("line", button.x, button.y, button.image:getDimensions())
+            lg.setColor(1, 1, 1)
+        end
     end
 end
 
 function buttons:update()
     for k, button in ipairs(self.buttons) do
-        if isClicking(button.x, button.y, button.image:getDimensions()) then
+        if mouseOver(button.x, button.y, button.image:getDimensions()) and lmouse.isDown() then
             button.activate()
         end
     end
